587b06959d05b033bc0fe06a387939aeb14b3c86
Amanda Kom <mandareneekom@gmail.com>
2014-10-22 18:51:18 -0700

    cleaning up the complexities from Class 4 Exercise 5

--- lib/class4/exercise5.rb
+++ lib/class4/exercise5.rb
@@ -32,48 +32,32 @@
 # TIP #3: You only need to change the `modern_roman_numeral` method.
 
 # rubocop:disable MethodLength
+
 def modern_roman_numeral(num)
-  roman = ''
-  n = num
-
-  # ONE THOUSAND
-  return roman = 'M' if n == 1000
-
-  # HUNDREDS
-  if n.between?(900, 999)
-    roman += 'CM'
-  elsif n.between?(500, 899)
-    roman += 'D'
-    roman += 'C' * ((n / 100) % 5)
-  elsif n.between?(400, 499)
-    roman += 'CD'
-  else
-    roman += 'C' * ((n / 100) % 10)
+  ar_to_rom = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ]
+  answer = []
+  ar_to_rom.each do |ar_to_roman|
+    arabic, roman = ar_to_roman
+    quotient = num / arabic
+    next if quotient == 0
+    answer.push(roman * quotient)
+    num %= arabic
   end
-
-  # TENS
-  if ((n / 10) % 10) == 9
-    roman += 'XC'
-  elsif ((n / 10) % 10).between?(5, 8)
-    roman += 'L'
-    roman += 'X' * ((n / 10) % 5)
-  elsif ((n / 10) % 10) == 4
-    roman += 'XL'
-  else
-    roman += 'X' * ((n / 10) % 10)
-  end
-
-  # ONES
-  if (n % 10) == 9
-    roman + 'IX'
-  elsif (n % 10).between?(5, 8)
-    roman += 'V'
-    roman + 'I' * (n % 5)
-  elsif (n % 10) == 4
-    roman + 'IV'
-  else
-    roman + 'I' * (n % 5)
-  end
+  answer.join
 end
 
 input = ARGV[0].to_i
